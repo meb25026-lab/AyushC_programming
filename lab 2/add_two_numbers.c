924f31bfbd0a00bffe09878dbdce0acb842034d2
meb25026-lab <meb25026@tezu.ac.in>
2025-11-21 23:20:37 +0530

    2nd lab

---
+++ lab 2/add_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a,b,sum;
+    printf("a:");
+    scanf("%f",&a);
+    printf("b;");
+    scanf("%f",&b);
+    sum=a+b;
+    printf("summation value is:%f",sum);
+    return 0;
+}
---
+++ lab 2/force_calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a,b,force;
+    printf("the mass is:");
+    scanf("%f",&a);
+    printf("acceleration is :");
+    scanf("%f",&b);
+    force=a*b;
+    printf("the force is %f\n",force);
+
+}
